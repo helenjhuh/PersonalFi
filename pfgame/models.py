dae1fecc067d0644a5d7d7d2aedecac699325d65
Bill Dengler <codeofdusk@gmail.com>
2020-04-01 18:04:43 -0400

    Add cc_overdue_balance and update code owners.

--- pfgame/models.py
+++ pfgame/models.py
@@ -8,7 +8,8 @@ class GameProfile(models.Model):
     bank_balance = models.IntegerField(default=0)
     investment_balance = models.IntegerField("Value of investments", default=0)
     property_balance = models.IntegerField("Value of property", default=0)
-    cc_balance = models.IntegerField("Credit card balance", default=0)
+    cc_balance = models.IntegerField("Credit card balance", default=0, help_text="The portion of credit card balance that does not accrue interest.")
+    cc_overdue_balance = models.IntegerField("Overdue credit card balance", default=0, help_text="The portion of credit card balance that accrues interest.")
     mortgage_balance = models.IntegerField(default=0)
     wage = models.IntegerField(
         default=0, help_text="Paid fortnightly, 0 if unemployed."
