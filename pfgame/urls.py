c01888a7e7efc70bdff5695f0bbd0c497efbe458
Bill Dengler <codeofdusk@gmail.com>
2020-04-20 14:46:44 -0400

    Add support for buying items and an advance button for the simulation. (#16)
    ### Link to issue number:
    None.
    
    ### Summary of the pull request:
    This PR adds the ability to buy items that have been defined in the admin, either with bank balance or credit card. It also adds a button for advancing the week counter (really a link to the main page styled to look like a button, @ckim5 you might want to see how I added this as a reference).
    
    ### Testing performed:
    * Opened the index page with no items in the database. Observed that a "no items for sale" message was displayed.
    * Added an item to the database and attempted to buy with bank. The bank balance was subtracted.
    * Added an item to the database and attempted to buy with credit card. The credit card balance was increased.
    * Overdrew the bank account. Observed that the shop section disappeared.
    * Clicked the "do nothing this week" button. Observed that the week number increased and a simulation step was performed.
    
    ### Known issues with pull request:
    * Since the database doesn't store the quantity owned of each item, attempting to buy an item twice just charges twice. We might want to hide already-purchased items.
    * A list of items a user purchased is not displayed anywhere (but it is tracked in the database). This is probably fine for an MVP, but I can open a separate issue if this is worth looking into.
    * There is no penalty for running a very high credit card balance. In reality a card would be frozen after several (two plus) missed payments. I can open a separate issue if this is worth fixing.

--- pfgame/urls.py
+++ pfgame/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import IndexView, InvestView, DivestView, CCPaymentView
+from .views import IndexView, InvestView, DivestView, CCPaymentView, BuyView
 
 urlpatterns = [
     path("", IndexView.as_view(), name="index"),
     path("invest/", InvestView.as_view(), name="invest"),
     path("divest/", DivestView.as_view(), name="divest"),
     path("cc/", CCPaymentView.as_view(), name="CCPayment"),
+    path("buy/<int:pk>/", BuyView.as_view(), name="buy"),
 ]
